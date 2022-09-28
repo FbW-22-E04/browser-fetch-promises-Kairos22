74c5a7a40eff10015a3f79879e02a3d4abf5db57
Kairos22 <alandiko84@gmail.com>
2022-09-28 18:25:42 +0200

    Ex1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,21 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
 // Import any additional modules you want to include below \/
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
 
 
 // \/ All of your javascript should go here \/
+const promiseFunction = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, 6000);
+});
 
+promiseFunction.then(() => {
+  modal.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  modal.style.display = "none";
+});
